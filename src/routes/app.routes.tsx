f38eba24c39dee96ed9b00481fc15213934513c5
lilicrst <lisandra.crisilva37@gmail.com>
2023-02-13 17:14:39 -0300

    feat: Bottom Navigator

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,43 @@
+import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+
+import { Home } from '@screens/Home';
+import { History } from '@screens/History';
+import { Exercise } from '@screens/Exercise';
+import { Profile } from '@screens/Profile';
+
+type AppRoutes = {
+  home: undefined;
+  exercise: undefined;
+  profile: undefined;
+  history: undefined;
+}
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{
+      headerShown: false, 
+      tabBarShowLabel: false,
+      }}>
+      <Screen 
+        name='home'
+        component={Home}
+      />
+      <Screen 
+        name='history'
+        component={History}
+      />
+      <Screen 
+        name='profile'
+        component={Profile}
+      />
+      <Screen 
+        name='exercise'
+        component={Exercise}
+      />
+    </Navigator>
+  )
+}
