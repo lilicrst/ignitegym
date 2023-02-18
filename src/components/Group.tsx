a27e937f6fd0e9056c94feacb15e0aa8732588bd
lilicrst <lisandra.crisilva37@gmail.com>
2023-02-17 21:22:51 -0300

    feat: Group component

---
+++ src/components/Group.tsx
@@ -0,0 +1,34 @@
+import { Text, Pressable, IPressableProps } from "native-base";
+
+type Props = IPressableProps & {
+  name: string;
+}
+
+export function Group({ name, ...rest }: Props) {
+  return (
+    <Pressable
+      mr={3}
+      w={24}
+      h={10}
+      bg="gray.600"
+      rounded="md"
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+      _pressed={{
+        borderWidth: 1,
+        borderColor: "green.500"
+      }}
+      {...rest}
+    >
+      <Text
+        color="gray.200"
+        textTransform="uppercase"
+        fontSize="xs"
+        fontWeight="bold"
+      >
+        {name}
+      </Text>
+    </Pressable>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,17 @@
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack } from 'native-base';
+import { HStack, VStack } from 'native-base';
 
-export function Home(){
+export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <HStack>
+        <Group name='costas' />
+        <Group name='ombro' />
+      </HStack>
+
     </VStack>
   );
 }
