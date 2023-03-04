28c1c71d8d34b2c39d5e2116802d96c5f1fa0e9f
lilicrst <lisandra.crisilva37@gmail.com>
2023-03-04 12:31:26 -0300

    chore: use ImagePicker, use expo FileSystem

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 import { ScreenHeader } from '@components/ScreenHearder';
 import { UserPhoto } from '@components/UserPhoto';
@@ -12,15 +13,47 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/lilicrst.png');
 
-  async function handleUserPhotoSelect(){
-    await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true
-    });
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+
+    try {
+      setPhotoIsLoading(true);
+
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+
+        if(photoInfo.size && (photoInfo.size / 1024 / 1024 > 5)) {
+          return toast.show({
+            title: "Use uma imagem de até 5MB",
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }      
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
   }
+
 
   return (
     <VStack flex={1}>
@@ -40,7 +73,7 @@ export function Profile() {
               />
               :
               <UserPhoto
-                source={{ uri: 'https://github.com/lilicrst.png' }}
+                source={{ uri: userPhoto }}
                 alt='Foto do usuário'
                 size={PHOTO_SIZE}
               />
@@ -54,11 +87,11 @@ export function Profile() {
 
           <Input
             placeholder='Nome'
-            bg='gray.600'            
+            bg='gray.600'
           />
           <Input
             placeholder='E-mail'
-            bg='gray.600'            
+            bg='gray.600'
             isDisabled
           />
         </Center>
@@ -68,23 +101,23 @@ export function Profile() {
             Alterar senha
           </Heading>
 
-          <Input 
+          <Input
             placeholder='Senha antiga'
             bg='gray.600'
             secureTextEntry
           />
-          <Input 
+          <Input
             placeholder='Nova senha'
             bg='gray.600'
             secureTextEntry
           />
-          <Input 
+          <Input
             placeholder='Confirme a nova senha'
             bg='gray.600'
             secureTextEntry
           />
 
-          <Button 
+          <Button
             title='Atualizar'
             mt={4}
           />
