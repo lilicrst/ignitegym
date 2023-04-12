6aa4d3a6e737ee7b05e7b756c142b7467ca63530
lilicrst <lisandra.crisilva37@gmail.com>
2023-04-12 17:23:20 -0300

    feat: use Interceptiors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://10.0.0.5:3333'
 });
+
+api.interceptors.request.use((config) => {
+  console.log("Dados da requisição", config.data)
+  
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export { api };
