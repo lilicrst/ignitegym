8ef6c6216a2451fecf05c0549a30e8b3112a4999
lilicrst <lisandra.crisilva37@gmail.com>
2023-04-12 22:00:33 -0300

    chore: error handling

--- src/screens/SingUp.tsx
+++ src/screens/SingUp.tsx
@@ -1,18 +1,17 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
 import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import axios from 'axios';
 import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
-import { Alert } from 'react-native';
 
 type FormDataProps = {
   name: string;
@@ -30,6 +29,8 @@ const signUpSchema = yup.object({
 
 export function SingUp() {
 
+  const toast = useToast();
+
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema)
   });
@@ -45,9 +46,14 @@ export function SingUp() {
       const response = await api.post('/users', { name, email, password });
       console.log(response.data);
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        Alert.alert(error.response?.data.message);
-      }
+      const  isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
     }
   }
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,16 @@
+import { AppError } from "@utils/AppError";
 import axios from "axios";
 
 const api = axios.create({
   baseURL: 'http://10.0.0.5:3333'
 });
 
-api.interceptors.request.use((config) => {
-  console.log("Dados da requisição", config.data)
-  
-  return config;
-}, (error) => {
-  return Promise.reject(error);
+api.interceptors.request.use(response => response, error => {
+  if(error.response && error.response.data){
+    return Promise.reject(new AppError(error.response.data.message));
+  } else {
+    return Promise.reject(new AppError(error));
+  }
 });
 
 export { api };
